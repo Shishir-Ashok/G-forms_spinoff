dd29c63076bb5bb6ddd781854cd86523bcf018bb
Anand Patil <anand@preceptlabs.com>
2020-06-02 17:32:07 +0530

    background img fix

--- src/Pages/LandingPage/Content.jsx
+++ src/Pages/LandingPage/Content.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import "./Content.scss";
 
 export default function Content() {
-    return (
-        <div className="Main">
-            <div className="Content">
-                <img src='/assets/svg/Rectangle_main.svg' className="Content-main_rect" alt="Main rectangle" />
-            
-                <div className="welcome">
-                    <img src='/assets/svg/Square_main.svg' className="Content-main_square" alt="Main rectangle" />
-                    <h1 className="welcomeHeader">Welcome to Forms</h1>
-                    <button className="getStarted">Get Started</button>
-                    
-                </div>
-                
-            </div>
+  return (
+    <div className="Main">
+      <div className="Content">
+        <img
+          src="/assets/svg/Rectangle_main.svg"
+          className="Content-main_rect"
+          alt="Main rectangle"
+        />
+
+        <div className="welcome">
+          <div className="Content-main_square" alt="Main rectangle" />
+          <h1 className="welcomeHeader">Welcome to Forms</h1>
+          <button className="getStarted">Get Started</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-                        
