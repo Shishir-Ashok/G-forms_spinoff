7ccf216f970ee1d5e51eacccf229e0f7ae7a4d18
Anand Patil <anand@preceptlabs.com>
2020-06-02 17:20:02 +0530

    signup added hooks

---
+++ src/Components/SignUp/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import useInput from "../../Hooks/useInput";
+
+export default function SignUp() {
+  const username = useInput("");
+  const email = useInput("");
+
+  return <div></div>;
+}
