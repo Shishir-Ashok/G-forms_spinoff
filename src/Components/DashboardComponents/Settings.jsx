34cc01447df8374a9de9c7ec4cbc461ca251e0d9
Anand Patil <anand.patil.ap79@gmail.com>
2020-06-12 20:19:25 +0530

    fix

--- src/Components/DashboardComponents/Settings.jsx
+++ src/Components/DashboardComponents/Settings.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import SignleLineInput from "../FormSections/SignleLineInput";
 
 export default function Settings() {
-    return (
-        <>
-            <SignleLineInput />
-        </>
-    );
+  return (
+    <div>
+      <SignleLineInput />
+    </div>
+  );
 }
